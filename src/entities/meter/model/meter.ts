f0ee89ec43769dcc162c1d22ef96ecd03ba4adb3
Arthur <arturka_tx@mail.ru>
2026-05-08 19:36:52 +0300

    feat: типы и MST-модель Meter

---
+++ src/entities/meter/model/meter.ts
@@ -0,0 +1,12 @@
+import { types } from 'mobx-state-tree';
+
+export const MeterModel = types.model('Meter', {
+  id: types.identifier,
+  _type: types.string,
+  installation_date: types.string,
+  is_automatic: types.boolean,
+  initial_values: types.array(types.number),
+  description: types.string,
+  areaId: types.string,
+  address: types.maybe(types.string),
+});
